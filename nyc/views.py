966552594b7d05a7373cfe69ec67a822db3e3797
Allison Kroboth <42.allison.k@gmail.com>
2022-04-07 07:45:16 -0400

    completed_views

--- nyc/views.py
+++ nyc/views.py
@@ -23,9 +23,21 @@ class ActivityView(View):
         return render(
         request=request,
         template_name='activity.html',
-        context={'borough': borough, 'activity': activity, 'venues':boroughs[borough][activity].keys()}
+        context={
+            'borough': borough, 'activity': activity, 
+            'venues':boroughs[borough][activity].keys()}
         )
 
 
 class VenueView(View):
-    pass
+    
+    def get(self, request, borough, activity, venue):
+        return render(
+            request=request,
+            template_name='venues.html',
+            context={
+                'borough':borough,
+                'venue': venue,
+                'description': boroughs[borough][activity][venue]['description']
+            }
+        )
